35e23e872e9de3a4d9095d4b85c27afb0c677dd3
rogierhans <rogierhans@gmail.com>
2017-02-20 13:25:43 +0100

    First COmmit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray genotype = new BitArray(100);
+            string aidsniels = "AidsNiels";
+           
+        }
+    }
+}
